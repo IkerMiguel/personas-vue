77f5bfccdff2044167e99453b0b859a4e3ea0037
IkerMiguel <ikmiguel03@gmail.com>
2025-05-19 00:04:04 -0500

    Add Delete Departamento

--- src/views/Departamento.vue
+++ src/views/Departamento.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class="container">
-    <h1 class="text-start">Listado Departamentos |
-      <button @click="newDepartamento" class="btn btn-success mx-2">
-        <font-awesome-icon icon="plus" />
-      </button>
-    </h1>
-    <table class="table">
-      <thead>
-        <tr>
-          <th class="col">#</th>
-          <th class="col">Code</th>
-          <th class="col">Name</th>
-          <th class="col">Country</th>
-          <th class="col">Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(departamento, index) in departamentos" :key="index">
-          <td>{{ index + 1 }}</td>
-          <td>{{ departamento.depa_codi }}</td>
-          <td>{{ departamento.depa_nomb }}</td>
-          <td>{{ departamento.pais_nomb }}</td>
-          <td>
-            <button @click="deleteDepartamento(departamento.depa_codi)" class="btn btn-danger mx-2">
-              <font-awesome-icon icon="trash" />
+    <div class="container">
+        <h1 class="text-start">Listado Departamentos |
+            <button @click="newDepartamento()" class="btn btn-success mx-2">
+                <font-awesome-icon icon="plus" />
             </button>
-            <button @click="editDepartamento(departamento.depa_codi)" class="btn btn-warning mx-2">
-              <font-awesome-icon icon="pencil" />
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+        </h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Code</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(departamento, index) in departamentos" :key="departamento.depa_codi">
+                    <th scope="row">{{ index + 1 }}</th>
+                    <td>{{ departamento.depa_codi }}</td>
+                    <td>{{ departamento.depa_nomb }}</td>
+                    <td>{{ departamento.pais_nomb }}</td>
+                    <td>
+                        <button @click="deleteDepartamento(departamento.depa_codi)" class="btn btn-danger mx-2">
+                            <font-awesome-icon icon="trash" />
+                        </button>
+                        <button @click="editDepartamento(departamento.depa_codi)" class="btn btn-warning mx-2">
+                            <font-awesome-icon icon="pencil" />
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -40,18 +41,38 @@ import axios from 'axios'
 import Swal from 'sweetalert2'
 
 export default {
-  name: 'Departamento',
-  data() {
-    return {
-      departamentos: []
-    }
-  },
-  methods: {
-    
-  },
-  mounted() {
-    axios.get('http://127.0.0.1:8000/api/departamentos')
-      .then(response => (this.departamentos = response.data.departamentos))
-  }
+    name: 'Departamento',
+    data() {
+        return {
+            departamentos: []
+        }
+    },
+    methods: {
+        deleteDepartamento(codigo) {
+            Swal.fire({
+                title: `Do you want to delete the Comuna with id  ${codigo}?`,
+                showCancelButton: true,
+                confirmButtonText: 'Delete',
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    axios.delete(`http://127.0.0.1:8000/api/departamentos/${codigo}`)
+                        .then(response => {
+                            if (response.data.success) {
+                                Swal.fire('Deleted! ', '', 'success')
+                                this.departamentos = response.data.departamentos
+                            }
+                        })
+                    
+                    }
+                })
+            },
+    },
+    mounted(){
+        axios
+            .get('http://127.0.0.1:8000/api/departamentos')
+            .then(response =>(this.departamentos = response.data.departamentos))
+    },
 }
+
+
 </script>
